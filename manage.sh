d102507641da2d3a9651052c573a694d699ea2d3
benibela <benito@benibela.de>
2014-10-10 23:08:48 +0200

    update mirrors

--- manage.sh
+++ manage.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../../manageUtils.sh
 
-githubProject subtitleviewer
+mirroredProject subtitleviewer
 
 BASE=$HGROOT/programs/data/subtitleviewer
 
